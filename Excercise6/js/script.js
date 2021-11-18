112c9a32a5ab1fd9b1127fa4416e4ce77d851d00
nguyenanbinh <nguyenanbinh@gmail.com>
2021-11-18 08:20:00 +0700

    fix validate phone/mail, checkbox select all, text Delete -> Action

--- Excercise6/js/script.js
+++ Excercise6/js/script.js
@@ -3,15 +3,22 @@ const table = document.getElementById('POITable');
 const cell = document.getElementsByTagName('td');
 const row = document.getElementsByTagName('tr');
 var rowLength = row.length;
-
-cellEditable("dblclick", true);
+const inputPhone = document.getElementsByClassName("phone");
+const inputMail = document.getElementsByClassName("mail");
+cellEditable("click", true);
 cellEditable("focusout", false);
+
+function checkValidate(regex, text) {
+    if (!regex.test(text)) {
+        alert("Invalid field");
+    }
+}
 
 function deleteOneRow(row) {
     var i = row.parentNode.parentNode.rowIndex;
     document.getElementById('POITable').deleteRow(i);
     rowLength--;
-    cellEditable("dblclick", true);
+    cellEditable("click", true);
 }
 
 function deleteRows() {
@@ -19,8 +26,12 @@ function deleteRows() {
     const tbody = tableRef.querySelector("tbody");
     const checkedInputs = document.querySelectorAll('input[name="name[]"]:checked');
     Array.prototype.slice.call(checkedInputs)
-        .forEach(input => tbody.removeChild(input.parentNode.parentNode));
-    cellEditable("dblclick", true);
+        .forEach(input => {
+            tbody.removeChild(input.parentNode.parentNode);
+            rowLength--;
+        });
+    cellEditable("click", true);
+    document.getElementById("select_all").checked = false;
 }
 
 function insRow() {
@@ -34,20 +45,33 @@ function insRow() {
     firstCell.innerHTML = `<input type="checkbox" name="name[]" id="${len}" />`;
     secondCell.innerHTML = '<p style="width: 200px;" class="name"></p>';
     thirdCell.innerHTML = '<p style="width: 200px;" class="phone"></p>';
-    fourthCell.innerHTML = '<p style="width: 200px;" class="birthday"></p>';
+    fourthCell.innerHTML = '<p style="width: 200px;" class="mail"></p>';
     fifthCell.innerHTML = '<input type="button" class="delPOIbutton" value="Delete" onclick="deleteOneRow(this)" />';
     rowLength++;
-    cellEditable("dblclick", true);
+    cellEditable("click", true);
     cellEditable("focusout", false);
 }
 
 function cellEditable(event, editStatus) {
-    for (let indexRow = 0; indexRow < rowLength; indexRow++) {
-        for (let indexCell = 0; indexCell < cell.length; indexCell++) {
-            cell[indexCell].addEventListener(event, function() {
-                cell[indexCell].contentEditable = editStatus;
-            });
-        }
+    const regexPhone = /^[0][0-9]{9}/;
+    const regexEmail =
+        /^(([^<>()[\]\\.,;:\s@#\"]+(\.[^<>()[\]\\.,;:#\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const nameRegex = /^[a-zA-Z\s]*$/;
+    for (let indexCell = 1; indexCell < cell.length - 1; indexCell++) {
+        cell[indexCell].addEventListener(event, function() {
+            cell[indexCell].contentEditable = editStatus;
+            if (event == "focusout") {
+                if ((cell[indexCell].children[0]).classList.contains("phone")) {
+                    checkValidate(regexPhone, cell[indexCell].textContent);
+                }
+                if ((cell[indexCell].children[0]).classList.contains("mail")) {
+                    checkValidate(regexEmail, cell[indexCell].textContent);
+                }
+                if ((cell[indexCell].children[0]).classList.contains("name")) {
+                    checkValidate(nameRegex, cell[indexCell].textContent);
+                }
+            }
+        });
     }
 }
 
